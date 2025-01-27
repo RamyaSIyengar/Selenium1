8a0420ceff9982cfeaa0a7875a2c96e42346ab93
Ramya S <77056918+RamyaSIyengar@users.noreply.github.com>
2025-01-28 04:22:20 +0530

    Update test_fixture.py

--- pytest/test_fixture.py
+++ pytest/test_fixture.py
@@ -24,3 +24,20 @@ def test_user(self, user_data):
     assert isinstance(name, str)
     assert isinstance(age, int)
 
+
+@pytest.fixture(scope='class')
+def setup(request):
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://testautomationpractice.blogspot.com/")
+    driver.maximize_window()
+    request.cls.driver = driver  # Assign the driver to the test class
+    yield driver
+    driver.quit()
+
+
+@pytest.mark.usefixtures("setup")
+class TestPy:
+     def test_login(self,setup):
+        self.driver = setup
+        assert "Automation" in self.driver.title
+
