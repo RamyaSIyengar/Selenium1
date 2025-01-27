a3bc07787c80232b0b9272b5221678c2f0b7413b
Ramya S <77056918+RamyaSIyengar@users.noreply.github.com>
2025-01-28 04:21:03 +0530

    Update test_fixture.py

--- pytest/test_fixture.py
+++ pytest/test_fixture.py
@@ -13,3 +13,14 @@ def test_divisible_by_3(input_value):
 
 def test_divisible_by_6(input_value):
     assert input_value % 6 == 0
+
+# Fixtures can be parameterized to run the same test with different data.
+@pytest.fixture(params=[('alia',35),('deepika',40)])
+def user_data(self,request):
+    return request.param
+
+def test_user(self, user_data):
+    name,age = user_data
+    assert isinstance(name, str)
+    assert isinstance(age, int)
+
